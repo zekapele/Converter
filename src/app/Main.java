098b051fd5bc5e7538bbdd0ab27dca44ed45fb7b
jekapele <zekapele@gmail.com>
2025-03-01 15:22:05 +0200

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+
+public class Main {
+
+    private static final double CONV_K = 2.20462; // Коефіцієнт для конвертації
+
+    public static void main(String[] args) {
+        System.out.println("Converter App.");
+
+        double kgs = 5; // Вага в кілограмах
+        double pounds = convKgsToPounds(kgs);
+        System.out.println("Result is " + pounds + " pounds.");
+    }
+
+    private static double convKgsToPounds(double kgs) {
+        return kgs * CONV_K;
+    }
+}
