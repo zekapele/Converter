a807e8dab88e4db59478f88665c1fe4b4f3602a0
jekapele <zekapele@gmail.com>
2025-03-01 16:59:31 +0200

    Added reverse conversion

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,17 +2,25 @@ package app;
 
 public class Main {
 
-    private static final double CONV_K = 2.20462; // Коефіцієнт для конвертації
+    private static final double CONV_K = 2.20462;
 
     public static void main(String[] args) {
-        System.out.println("Converter App.");
+        System.out.println("App for measures converting.");
+        System.out.println("Version 1.0.");
 
-        double kgs = 5; // Вага в кілограмах
+        double kgs = 5;
+        double pnds = 12;
         double pounds = convKgsToPounds(kgs);
-        System.out.println("Result is " + pounds + " pounds.");
+        double kilos = convPoundsToKgs(pnds);
+
+        System.out.println("Result is " + pounds + " pounds and " + kilos + " kgs.");
     }
 
     private static double convKgsToPounds(double kgs) {
         return kgs * CONV_K;
+    }
+
+    private static double convPoundsToKgs(double pnds) {
+        return pnds / CONV_K;
     }
 }
